daa9c308e1c4983e3d57342cdb3e491fe796369c
VissZyCrain <aletuchin@yandex.ru>
2022-12-21 20:06:01 +0300

    realized task 2

--- task 2.py
+++ task 2.py
@@ -6,13 +6,9 @@ os.system('cls')
 
 print('Задача № 2')
 
-bools = ['12', 'sadf', '5643', 'vvbds']
-
-out = filter(None, bools)
-
-for iter in out:
-    print(iter)
-
-
-
-# print('Список уникальных элементов: ', list )
+new_list = [12, 'sadf', 5643]
+list2 = []
+list2 = list(filter(lambda x: type(x) == int, new_list))
+list3 = list(filter(lambda x: type(x) == str, new_list))
+print('Числа в списке:', list2)
+print('Строки в списке:', list3)
